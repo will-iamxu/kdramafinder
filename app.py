b789f92498676f5193b864c5306d36ecc0500cf1
William Xu <BillXu321@gmail.com>
2023-12-27 13:45:45 -0800

    Fix undefined api_key issue

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import requests
 import os
 app = Flask(__name__)
-
+api_key = os.getenv('api_key')
 def get_actor_id(actor_name, api_key):
     response = requests.get(f"https://api.themoviedb.org/3/search/person",
                             params={"api_key": api_key, "query": actor_name})
@@ -53,5 +53,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    api_key = os.getenv('api_key')
+    
     app.run(debug=True)
